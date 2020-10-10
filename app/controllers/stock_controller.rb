eef2bf76e23d4eff24b226f161633370372ee25d
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-10-09 22:14:22 -0400

    remove spacing

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -9,7 +9,6 @@ get "/stocks" do
 end
 
 post "/stocks" do
-#  binding.pry
   already_in = Stock.where("user_id=#{session[:user_id]}").any? {|stock| stock.ticker == "#{params[:search].upcase}"}
 
   if already_in
@@ -48,14 +47,12 @@ patch "/update" do
   params.delete("_method")
   @stock= Stock.find_by(ticker: "#{params.keys.first}")
   updated_price=Stock.create_company("#{params.keys.first}")[:price]
-  # binding.pry
   @stock.update(price: updated_price)
   redirect "/stocks"
 
 end
 
 patch "/stocks/:id" do
-  # binding.pry
   @stock =Stock.find(params[:id])
   params.delete("_method")
   @stock.update(params)
